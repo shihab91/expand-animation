c4f138f58f90d3ce17cef919cfd8495989dca838
shihab shumon <shumonshihab62@gmail.com>
2022-03-25 22:34:45 +0600

    made an expandable card with framer motion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { motion } from "framer-motion";
+import { useState } from "react";
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div
+        layout
+        transition={{
+          layout: {
+            duration: 0.7,
+            type: "spring",
+          },
+        }}
+        className="card"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        <motion.h2 layout="position" style={{ fontSize: "30px" }}>
+          Shihab Shumon 🚀
+        </motion.h2>
+        {isOpen && (
+          <motion.div
+            className="expand"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.9 }}
+          >
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos perferendis minus mollitia facere error rem laboriosam
+              ratione aliquid vero. Ad.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia,
+              molestiae?
+            </p>
+          </motion.div>
+        )}
+      </motion.div>
     </div>
   );
 }
